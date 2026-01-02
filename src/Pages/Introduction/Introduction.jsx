af30f21a09ece667d6998743418edcbaed9ef93f
alphaovi <ovi778811@gmail.com>
2026-01-03 02:45:04 +0600

    introduction css modify

--- src/Pages/Introduction/Introduction.jsx
+++ src/Pages/Introduction/Introduction.jsx
@@ -8,25 +8,32 @@ const Introduction = () => {
       <h1 className="text-7xl  text-white font-bold ">Full Stack</h1>
       <p className="mt-2 text-7xl font-bold text-brand-gradient">Developer</p>
       <div className="mt-2 text-2xl text-white">
-      <Typewriter
-      
-        options={{
-          strings: ["Tech Enthusiast", "Computer Science & Engg. Student"],
-          autoStart: true,
-          loop: true,
-        }}
-      />
-    </div>
+        <Typewriter
+          options={{
+            strings: ["Tech Enthusiast", "Computer Science & Engg. Student"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </div>
       <p className="mt-2 text-gray-500 text-xl">
         Enhancing digital experiences that are smooth, scalable, and made to
         impress.
       </p>
 
       <div className="mt-5 flex gap-5">
-        <button className="btn rounded-full ">React</button>
-        <button className="btn rounded-full ">Javascript</button>
-        <button className="btn rounded-full ">Node.js</button>
-        <button className="btn rounded-full ">Mongodb</button>
+        <button className="btn rounded-full bg-[#161725] border-none text-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110  ">
+          React
+        </button>
+        <button className="btn rounded-full bg-[#161725] border-none text-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 ">
+          Javascript
+        </button>
+        <button className="btn rounded-full bg-[#161725] border-none text-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 ">
+          Node.js
+        </button>
+        <button className="btn rounded-full bg-[#161725] border-none text-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 ">
+          Mongodb
+        </button>
       </div>
     </div>
   );
